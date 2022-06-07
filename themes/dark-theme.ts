f89b0c1e46034513290ef34ccc9d25bcd6bb82c3
Julian Martinez <julian.fmartinez@gmail.com>
2022-06-07 00:39:20 -0300

    init: Se agrega MUI a las dependecias, se configuran temas principales

---
+++ themes/dark-theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+import { grey, red } from '@mui/material/colors';
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+
+  components: {
+    
+  }
+});
